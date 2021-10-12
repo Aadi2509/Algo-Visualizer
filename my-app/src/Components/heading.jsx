84eaedb589623df40e8d4e40c277151e66e6f49c
unknown <ayushsalvi19@gmail.com>
2021-10-13 01:02:16 +0530

    Create Landing Page

---
+++ my-app/src/Components/heading.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+function heading(){
+
+return (<div className="Heading">
+<div>
+<h1 className="typing1" >Algorithm Visualizer </h1>
+
+{/* <h1 className="typing" >Visualizer</h1> */}
+</div> 
+</div>);
+
+}
+
+export default heading;
